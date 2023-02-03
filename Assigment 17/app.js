8176dfaa8dbeacff16fcfeeb832692163321aea2
M-Muiz <122803576+M-Muiz@users.noreply.github.com>
2023-02-04 00:52:28 +0500

    Add files via upload

---
+++ Assigment 17/app.js
@@ -0,0 +1,42 @@
+Q:
+var a = (55);
+
+var b = (45);
+
+var c = (a + b);
+
+document.write ("<h1>The sum of  55 and 45 is ")
+document.write (c)
+
+
+Q:
+
+var d = (a - b);
+
+document.write ("<br>The subtract of  55 and 45 is ")
+document.write (d)
+
+Q:
+
+var e = (a / b);
+
+document.write ("<br>The divison of  55 and 45 is ")
+document.write (e)
+
+Q:
+
+var f = (a * b);
+
+document.write ("<br>The multiplaication of  55 and 45 is ")
+document.write (f)
+
+Q:
+
+var g = (600);
+
+var h = (5);
+
+var j = (g*h); 
+
+document.write("<br>Total cost to buy 5 tickets to a movie is ")
+document.write (j)
